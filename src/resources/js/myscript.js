b4fafc226838ee6f186c1b165684e7fb6470385f
unknown <vusal.mirzeliyev@gmail.com>
2020-08-09 20:21:31 +0400

    Afc

---
+++ src/resources/js/myscript.js
@@ -0,0 +1,19 @@
+$('.carousel-news').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    pagination:true,
+    autoPlay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
